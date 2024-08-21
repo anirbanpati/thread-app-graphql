dc17ccadb14390482531d15dec18656f5988e3ec
Anirban Pati <anirbanpati55@gmail.com>
2024-08-21 08:20:21 +0530

    prisma setup done

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import  express  from "express";
 import { ApolloServer } from "@apollo/server";
 
 import {expressMiddleware} from '@apollo/server/express4'
+import { prismaClient} from './lib/db';
 
 async function init(){
 const app = express();
@@ -17,11 +18,33 @@ const gqlServer = new ApolloServer({
         say(name: String): String
     }
 
+    type Mutation {
+        createUser(firstName:String!,lastName: String!,email: String!, password: String!) : Boolean
+    }
+
     `,
     resolvers: {
         Query: {
             hello: () => 'Hello World',
             say: (_,{name}:{name:string}) => `Hello ${name}`
+        },
+        Mutation: {
+            createUser: async (_,{firstName,lastName,email,password}:
+                {firstName:string;lastName:string;email:string;password:string}) =>
+                     {
+
+                        await prismaClient.user.create({
+                            data: {
+                                firstName,
+                                lastName,
+                                email,
+                                password,
+                                salt: 'randomSalt'
+                            }
+                        });
+                        return true;
+
+            }
         }
 
     }
